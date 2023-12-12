899e9b2318d89aac98d96cdb8f62c1c16309b69e
Daniel <owenjodaniel19@gmail.com>
2023-12-12 21:27:40 +0300

    hero slider

--- src/components/UI/HeroSlider.jsx
+++ src/components/UI/HeroSlider.jsx
@@ -22,11 +22,11 @@ const HeroSlider = () => {
       <div className="slider__item slider__item-01 mt0">
         <Container>
           <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent Ksh4000 Per Hour</h4>
-            <h1 className="text-light mb-4">Reserve Now and Get 10% Off</h1>
+            {/* <h4 className="text-light mb-3">For Rent Ksh4000 Per Hour</h4> */}
+            <h1 className="text-light mb-4">A reputable institution offering <br /> quality and holistic education</h1>
 
             <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reserve Now</Link>
+              <Link to="/about">Learn More</Link>
             </button>
           </div>
         </Container>
@@ -35,11 +35,11 @@ const HeroSlider = () => {
       <div className="slider__item slider__item-02 mt0">
         <Container>
           <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent Ksh4000 Per Hour</h4>
-            <h1 className="text-light mb-4">Reserve Now and Get 10% Off</h1>
+            {/* <h4 className="text-light mb-3">For Rent Ksh4000 Per Hour</h4> */}
+            <h1 className="text-light mb-4">Encouraging Curious Minds <br /> To Reach Full Potential</h1>
 
             <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reserve Now</Link>
+              <Link to="/about">Learn More</Link>
             </button>
           </div>
         </Container>
@@ -48,11 +48,11 @@ const HeroSlider = () => {
       <div className="slider__item slider__item-03 mt0">
         <Container>
           <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent Ksh4000 Per Hour</h4>
-            <h1 className="text-light mb-4">Reserve Now and Get 10% Off</h1>
+            {/* <h4 className="text-light mb-3">For Rent Ksh4000 Per Hour</h4> */}
+            <h1 className="text-light mb-4">Providing a Safe, <br /> Secure and Nuturing Environment</h1>
 
             <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reserve Now</Link>
+              <Link to="/about">Learn More</Link>
             </button>
           </div>
         </Container>
